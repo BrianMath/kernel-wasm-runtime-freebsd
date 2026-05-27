123e0c9a8502c3cb5671c95ab5c56426438ab1d2
BrianMath <brian@ufg.br>
2026-05-27 11:01:55 -0300

    feat: host function

--- source/funcs/add/src/lib.rs
+++ source/funcs/add/src/lib.rs
@@ -1,4 +1,15 @@
+unsafe extern "C" {
+    fn sum_c(x: i32, y: i32) -> i32;
+}
+
 #[unsafe(no_mangle)]
 pub extern "C" fn add(left: u64, right: u64) -> u64 {
     left + right
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn sum(left: i32, right: i32) -> i32 {
+    unsafe {
+        return sum_c(left, right);
+    }
 }
--- source/wasmodule.c
+++ source/wasmodule.c
@@ -7,12 +7,24 @@
 #include <sys/kernel.h> /* types used in module initialization */
 
 #include "wasm3.h"
+#include "m3_env.h"
 #include "add.h"
 #include "fib.h"
 
+m3ApiRawFunction(host_sum) {
+    m3ApiReturnType(int32_t);
+    m3ApiGetArg(int32_t, a);
+    m3ApiGetArg(int32_t, b);
+    
+    int32_t c = a + b;
+    printf("a = %d, b = %d, c = %d\n", a, b, c);
+    
+    m3ApiReturn(c);
+}
+
 int loader() {
     M3Result result;
-    M3Result result2;
+    // M3Result result2;
 
     /* 2. Criar o ambiente Wasm3 */
     IM3Environment env = m3_NewEnvironment();
@@ -21,11 +33,11 @@ int loader() {
         return 1;
     }
 
-    IM3Environment env2 = m3_NewEnvironment();
-    if (!env2) {
-        printf("Erro ao criar o ambiente 2.\n");
-        return 1;
-    }
+    // IM3Environment env2 = m3_NewEnvironment();
+    // if (!env2) {
+    //     printf("Erro ao criar o ambiente 2.\n");
+    //     return 1;
+    // }
 
     /* 3. Criar o runtime. O segundo argumento é o tamanho da stack em bytes (ex: 8192) */
     IM3Runtime runtime = m3_NewRuntime(env, 8192, NULL);
@@ -35,15 +47,16 @@ int loader() {
         return 1;
     }
 
-    IM3Runtime runtime2 = m3_NewRuntime(env2, 8192, NULL);
-    if (!runtime2) {
-        printf("Erro ao criar o runtime 2.\n");
-        m3_FreeEnvironment(env2);
-        return 1;
-    }
+    // IM3Runtime runtime2 = m3_NewRuntime(env2, 8192, NULL);
+    // if (!runtime2) {
+    //     printf("Erro ao criar o runtime 2.\n");
+    //     m3_FreeEnvironment(env2);
+    //     return 1;
+    // }
 
     /* 4. Fazer o parse do binário WebAssembly */
     IM3Module module;
+
     result = m3_ParseModule(env, &module, add_wasm, add_wasm_len);
     if (result) {
         printf("Erro no parse: %s\n", result);
@@ -52,14 +65,14 @@ int loader() {
         return 1;
     }
 
-    IM3Module module2;
-    result2 = m3_ParseModule(env2, &module2, fib_wasm, fib_wasm_len);
-    if (result2) {
-        printf("Erro no parse: %s\n", result2);
-        m3_FreeRuntime(runtime2);
-        m3_FreeEnvironment(env2);
-        return 1;
-    }
+    // IM3Module module2;
+    // result2 = m3_ParseModule(env2, &module2, fib_wasm, fib_wasm_len);
+    // if (result2) {
+    //     printf("Erro no parse: %s\n", result2);
+    //     m3_FreeRuntime(runtime2);
+    //     m3_FreeEnvironment(env2);
+    //     return 1;
+    // }
 
     /* 5. Carregar o módulo no runtime */
     result = m3_LoadModule(runtime, module);
@@ -70,17 +83,25 @@ int loader() {
         return 1;
     }
 
-    result2 = m3_LoadModule(runtime2, module2);
-    if (result2) {
-        printf("Erro ao carregar o módulo 2: %s\n", result2);
-        m3_FreeRuntime(runtime2);
-        m3_FreeEnvironment(env2);
+    result = m3_LinkRawFunction(module, "env", "sum_c", "i(ii)", &host_sum);
+    if (result) {
+        printf("Erro no link: %s\n", result);
+        m3_FreeRuntime(runtime);
+        m3_FreeEnvironment(env);
         return 1;
     }
 
+    // result2 = m3_LoadModule(runtime2, module2);
+    // if (result2) {
+    //     printf("Erro ao carregar o módulo 2: %s\n", result2);
+    //     m3_FreeRuntime(runtime2);
+    //     m3_FreeEnvironment(env2);
+    //     return 1;
+    // }
+
     /* 6. (Opcional) Localizar e executar uma função exportada pelo módulo */
     IM3Function func;
-    result = m3_FindFunction(&func, runtime, "add");
+    result = m3_FindFunction(&func, runtime, "sum");
     if (!result) {
         /* A função é chamada. m3_CallV aceita argumentos variáveis baseados na assinatura */
 		printf("chamando função\n");
@@ -97,31 +118,31 @@ int loader() {
 		}
     }
 
-    IM3Function func2;
-    result2 = m3_FindFunction(&func2, runtime2, "fib");
-    if (!result2) {
-        /* A função é chamada. m3_CallV aceita argumentos variáveis baseados na assinatura */
-		printf("chamando função 2\n");
-        result2 = m3_CallV(func2, 10);
-        if (result2) {
-            printf("Erro na execução da função 2: %s\n", result2);
-        } else {
-			int retorno2;
-			printf("pegando resultado 2\n");
-			result2 = m3_GetResultsV(func2, &retorno2);
-			if (!result2) {
-				printf("Fib(10) = %d\n", retorno2);
-			}
-		}
-    }
+    // IM3Function func2;
+    // result2 = m3_FindFunction(&func2, runtime2, "fib");
+    // if (!result2) {
+    //     /* A função é chamada. m3_CallV aceita argumentos variáveis baseados na assinatura */
+	// 	printf("chamando função 2\n");
+    //     result2 = m3_CallV(func2, 10);
+    //     if (result2) {
+    //         printf("Erro na execução da função 2: %s\n", result2);
+    //     } else {
+	// 		int retorno2;
+	// 		printf("pegando resultado 2\n");
+	// 		result2 = m3_GetResultsV(func2, &retorno2);
+	// 		if (!result2) {
+	// 			printf("Fib(10) = %d\n", retorno2);
+	// 		}
+	// 	}
+    // }
 
     /* 7. Liberar recursos alocados.
        Nota: O módulo é liberado automaticamente quando o runtime ou environment é liberado. */
     m3_FreeRuntime(runtime);
     m3_FreeEnvironment(env);
 
-    m3_FreeRuntime(runtime2);
-    m3_FreeEnvironment(env2);
+    // m3_FreeRuntime(runtime2);
+    // m3_FreeEnvironment(env2);
 
     return 0;
 }
